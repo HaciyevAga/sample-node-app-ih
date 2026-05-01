58fee8ae825972099812373dd1a0022b923294c1
AgaHaciyev <haciyev.aga2006@gmail.com>
2026-05-01 16:49:22 +0400

    sonar-ci

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,21 @@
 module.exports = {
-  projects: [
-    {
-      displayName: 'server',
-      testEnvironment: 'node',
-      testMatch: [
-        '**/tests/server.test.js',
-        '**/tests/integration.test.js'
-      ],
-      collectCoverageFrom: [
-        'server.js',
-        '!**/node_modules/**',
-        '!**/coverage/**'
-      ],
-      coverageDirectory: 'coverage/server',
-      setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
-    },
-    {
-      displayName: 'client',
-      testEnvironment: 'jsdom',
-      testMatch: [
-        '**/tests/client.test.js'
-      ],
-      collectCoverageFrom: [
-        'public/script.js',
-        '!**/node_modules/**',
-        '!**/coverage/**'
-      ],
-      coverageDirectory: 'coverage/client',
-      setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
-    }
-  ]
-};
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.js',
+    '!node_modules/**',
+    '!coverage/**',
+    '!jest.config.js'
+  ],
+  coverageReporters: ['lcov', 'text', 'clover'],
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'coverage', outputName: 'junit.xml' }]
+  ],
+  testMatch: [
+    '**/tests/**/*.test.js',
+    '**/tests/**/*.spec.js'
+  ],
+  verbose: true
+}
